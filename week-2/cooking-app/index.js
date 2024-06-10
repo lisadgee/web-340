18f6aa89468a1f9065fd3fa2d689dbf2ec88f078
Lisa Gee <lisadgee@hotmail.com>
2024-06-09 21:24:02 -0400

    week 2 assignment

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,20 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Lisa Gee
+ * Date: 6/9/24
+ * File Name: index.js
+ * Description: Node - Week 2 Problem
 */
 
 // TODO: Import your module using require
+const {createRecipe, setTimer, quit} = require("./recipes");
+
 
 // TODO: Implement your CLI program here
+//Verify expected values
+function main() {
+   console.log(createRecipe(["Eggs", "Flour", "Butter"]));
+   console.log(setTimer(20));
+   console.log(quit());
+}
+
+main();
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,31 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Lisa Gee
+ * Date: 6/9/24
+ * File Name: recipes.js
+ * Description: Node - Week 2 Problem
 */
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+  let recipe = "Recipe created with ingredients: ";
+  let ingredientsList = "";
+  for (let ingredient of ingredients) {  //loop thru array of ingredients
+    ingredientsList = ingredientsList.concat(ingredient + ", "); //add each ingredient to list
+  }
+
+  //trim trailing characters
+  return recipe + ingredientsList.slice(0, -2);
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
-  // TODO: Implement this function
+  return "Timer set for " + minutes + " minutes";
 }
 
 // Define the quit function
 function quit() {
-  // TODO: Implement this function
+  return "Program exited"
 }
 
 // TODO: Export the functions
+module.exports = {createRecipe, setTimer, quit}
