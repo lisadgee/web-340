a7318369b6bf0a09d6f8f76ae51430d7972bf92d
Lisa Gee <lisadgee@hotmail.com>
2024-06-16 20:02:38 -0400

    submitting week 3 work for web-340

--- week-3/distance-calculator/src/distance-calculator.js
+++ week-3/distance-calculator/src/distance-calculator.js
@@ -1,5 +1,23 @@
+"use strict";
+//Object for storing AU planet-distance values
+const planetDistance = {
+  "Earth" : 1.0,
+  "Mars" : 1.5,
+  "Mercury" : 0.4,
+  "Venus": 0.7,
+  "Jupiter" : 5.2,
+  "Saturn" : 9.6,
+  "Uranus" : 19.2,
+  "Neptune": 30,
+  "Pluto": 39.5
+}
+//Function for calculating distance between planets
 function calculateDistance(planet1, planet2) {
-  // TODO: Implement this function
+  /* Used bracket notation to retrieve specific values from the planet-distance object.  Used 
+  absolute value function from math library to return positive distance value.  Used .toFixed to 
+  trim return value down to one decimal place.  Used parseFloat to convert string to a float.
+  */
+  return parseFloat(Math.abs(planetDistance[planet1] - planetDistance[planet2]).toFixed(1));
 }
 
-module.exports = calculateDistance;
+module.exports = calculateDistance;  //Make accessible
--- week-3/distance-calculator/test/distance-calculator.spec.js
+++ week-3/distance-calculator/test/distance-calculator.spec.js
@@ -1,8 +1,52 @@
-const assert = require('assert');
-const calculateDistance = require('../src/distance-calculator');
+//File that runs test for distance-calculator module.
+"use strict";
 
-function testFunctionDescription() {
-  // TODO: Implement this function
+const assert = require('assert'); //import assert library to run test
+const calculateDistance = require('../src/distance-calculator');  //import calculateDistance function
+
+//Unit test for distance from Earth to Mars
+function testEarthToMars() {
+  try {
+    //Verify return value is = expected value
+    assert.strictEqual(calculateDistance("Earth", "Mars"), 0.5);
+    console.info("Passed testEarthToMars");
+    return true;
+  } catch (error) {
+    //Catch and display errors thrown
+    console.error(`Failed testEarthToMars: ${error.message}`);
+    return false;
+  }
 }
 
-// Call your test functions here
+//Unit test for distance from Mars to Saturn
+function testMarsToSaturn() {
+  try {
+     //Verify return value is = expected value
+    assert.strictEqual(calculateDistance("Saturn", "Mars"), 8.1);
+    console.info("Passed testMarsToSaturn");
+    return true;
+  } catch (error) {
+    //Catch and display errors thrown
+    console.error(`Failed testMarsToSaturn: ${error.message}`);
+    return false;
+  }
+}
+
+//Unit test for distance from Saturn to Jupiter
+function testSaturnToJupiter() {
+  try {
+     //Verify return value is = expected value
+    assert.strictEqual(calculateDistance("Saturn", "Jupiter"), 4.4);
+    console.info("Passed testSaturnToJupiter");
+    return true;
+  } catch (error) {
+    //Catch and display errors thrown
+    console.error(`Failed testSaturnToJupiter: ${error.message}`);
+    return false;
+  }
+}
+
+// Call test functions 
+testEarthToMars();
+testMarsToSaturn();
+testSaturnToJupiter();
