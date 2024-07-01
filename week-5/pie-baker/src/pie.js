101385560d260828eb0a58dd117830ee63a38b52
Lisa Gee <lisadgee@hotmail.com>
2024-06-30 22:04:44 -0400

    week 5

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,30 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author: Lisa Gee
+ * Date: Jun 29, 2024
+ * File Name: pie.js
+ * Description: pie module
  */
 "use strict";
 
+// Function for baking pie with two parameters
 function bakePie(pieType, ingredients) {
-  // Your code here
+  //Essential ingredients
+  const essentialIngredients = [
+    "flour",
+    "sugar",
+    "butter"
+  ]
+  
+  //Verify the essential ingredient is in the list. If not, warn and exit
+  essentialIngredients.forEach(essentialIngredient => {
+    if (ingredients.includes(essentialIngredient) == false) {
+      console.log(`WARNING: ${essentialIngredient} not found in the list` )
+      process.exit(1);  
+    }
+  });
+
+  console.log(`${pieType} pie has been baked.` )
 }
+
 
 module.exports = { bakePie };
